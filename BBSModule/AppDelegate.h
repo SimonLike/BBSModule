9cec8ff247c072fc36eaa0e3e28a582673b9d146
SimonLike <simon@SimondeiMac.local>
2017-06-26 19:33:36 +0800

    first

---
+++ BBSModule/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  BBSModule
+//
+//  Created by Simon on 2017/6/26.
+//  Copyright © 2017年 Simon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
